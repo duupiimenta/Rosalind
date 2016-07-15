4a2799796965b9e61fe92c7c1f4abf7004c15a93
duupiimenta <eduardo.pimenta.bueno@outlook.com>
2016-07-15 05:06:29 -0300

    Complementing a Strand of DNA

---
+++ Complementing a Strand of DNA/Complementing a Strand of DNA/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complementing_a_Strand_of_DNA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dna = "";
+
+            dna = StringHelper.ReverseString(dna);
+            dna = dna.Replace("A", "Y");
+            dna = dna.Replace("T", "X");
+            dna = dna.Replace("Y", "T");
+            dna = dna.Replace("X", "A");
+            dna = dna.Replace("C", "Y");
+            dna = dna.Replace("G", "X");
+            dna = dna.Replace("Y", "G");
+            dna = dna.Replace("X", "C");
+
+            Console.WriteLine(dna);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Complementing a Strand of DNA/Complementing a Strand of DNA/StringHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complementing_a_Strand_of_DNA
+{
+    class StringHelper
+    {
+        /// Receives string and returns the string with its letters reversed.
+
+        public static string ReverseString(string s)
+        {
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+    }
+}
