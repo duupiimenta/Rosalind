a74331ff19492e752153a8ce490b053965afcd25
duupiimenta <eduardo.pimenta.bueno@outlook.com>
2016-07-15 03:51:52 -0300

    Transcribing DNA into RNA

---
+++ Transcribing DNA into RNA/Transcribing DNA into RNA/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transcribing_DNA_into_RNA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dna = "";            
+            string rna = dna.Replace("T", "U");
+
+            Console.WriteLine(rna);
+            Console.ReadKey();
+        }
+    }
+}
